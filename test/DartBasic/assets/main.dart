2be632037a2cb064eca8704e2d770b296e82cf3f
Rich E <rich.eakin@gmail.com>
2015-01-21 05:48:19 +0100

    added enum value to basic test

--- test/DartBasic/assets/main.dart
+++ test/DartBasic/assets/main.dart
@@ -1,10 +1,19 @@
 import 'cinder.dart';
 
-void main() {
+enum Fruit {
+	ORANGE,
+	APPLE,
+	BANANA
+}
+
+void main()
+{
   print("hello dart.");
   
   var answer = 23 * 4 / 2;
   print( "the answer is $answer" );
+
+	print( "index of BANANA: ${Fruit.BANANA.index}" );
 
 //  Color c = new Color( 1.0, 0.0, 0.0 );
 //  print( "r: ${c.r}, g: ${c.g}, b: ${c.b}, a: ${c.a}");
@@ -13,7 +22,8 @@ void main() {
     'radius' : 200.0,
     'segments' : 5,
     'color' : new Color( 1.0, 0.3, 0.15, 1.0 ),
-    'rotationRate' : 8.0
+    'rotationRate' : 8.0,
+	'fruit'        : Fruit.APPLE
   };
 
   toCinder( m );
