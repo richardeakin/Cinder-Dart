f66bc4910be80db45a998fbaa788e7ac40ee4181
Rich E <rich.eakin@gmail.com>
2015-01-21 22:51:59 -0800

    use a more suitable message in the script example

--- test/NativeCallback/assets/main.dart
+++ test/NativeCallback/assets/main.dart
@@ -3,5 +3,5 @@ void customCallback( String message ) native "customCallback";
 
 void main()
 {
-	customCallback( "A string message from the script." );
+	customCallback( "You can edit this in main.dart and hit 'r' to reload." );
 }
