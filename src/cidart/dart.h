d8782a6c6f595f48c71c3484012768f35cb688a3
Rich E <rich.eakin@gmail.com>
2015-06-13 23:34:56 -0400

    added cidart/dart.h umbrella header

---
+++ src/cidart/dart.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2015, Richard Eakin
+// Use of this source code (and the Dart VM) is governed by a
+// BSD-style license that can be found in the LICENSE.txt file.
+
+#pragma once
+
+#include "cidart/Script.h"
+#include "cidart/Types.h"
+#include "cidart/VM.h"
+#include "cidart/Debug.h"
