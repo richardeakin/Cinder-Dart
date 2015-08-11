73040d80b8c56263af26ad424165bf64db011f03
Marcus Pingel <marcus@marcuspingel.com>
2015-08-11 01:10:08 -0400

    Added support for vec4 and mat4 loading. Fixed typo in InfoMapApp.cpp.

--- src/cidart/cinder.dart
+++ src/cidart/cinder.dart
@@ -55,6 +55,12 @@ class ivec3 {
 	ivec3( this.x, this.y );
 }
 
+class vec4 {
+	num x, y, z, w;
+	
+	vec4( this.x, this.y, this.z, this.w );
+}
+
 class Color {
 	num r, g, b, a;
 
@@ -65,6 +71,15 @@ class Rect {
 	num x1, x2, y1, y2;
 
 	Rect( this.x1, this.y1, this.x2, this.y2 );
+}
+
+class mat4 {
+	num r1x, r2x, r3x, r4x, r1y, r2y, r3y, r4y, r1z, r2z, r3z, r4z, r1w, r2w, r3w, r4w;
+	
+	mat4( this.r1x, this.r2x, this.r3x, this.r4x, 
+		  this.r1y, this.r2y, this.r3y, this.r4y, 
+		  this.r1z, this.r2z, this.r3z, this.r4z, 
+		  this.r1w, this.r2w, this.r3w, this.r4w );
 }
 
 enum LogLevel {
